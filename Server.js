9f7166e12ddecd925e46bfaef7a673f450067e30
Mdknn <mdkaif123000@gmail.com>
2023-07-09 22:22:34 +0530

    first

--- Server.js
+++ Server.js
@@ -177,6 +177,6 @@ app.put('/image',(req,res) =>{
     .catch(err => res.status(400).json('not found'))
   
 })
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("logging")
 })
